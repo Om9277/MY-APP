9cb3ddce6a0d50491ba9e341283fcedff60da41e
Om9277 <suvarnkarom@gmail.com>
2026-02-27 22:37:18 +0530

    Intial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+ HEAD
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
@@ -69,6 +69,6 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
-=======
+
 # my-react-project
 >>>>>>> b37b32dd52350fa001334c3319736b1f2a964ee0
--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const themeStyle = {
+    backgroundColor: darkMode ? "#121212" : "#f4f4f4",
+    color: darkMode ? "white" : "black",
+    height: "100vh",
+    textAlign: "center",
+    paddingTop: "50px",
+    transition: "0.3s",
+  };
+
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>My First React Project</h1>
-      <p>Welcome to my institutional React project!</p>
+    <div style={themeStyle}>
+      <h1>My React is Working 🚀</h1>
+      <p>Welcome to my React project!</p>
+
+      <button
+        onClick={() => setDarkMode(!darkMode)}
+        style={{
+          padding: "10px 20px",
+          marginTop: "20px",
+          cursor: "pointer",
+          borderRadius: "5px",
+          border: "none",
+        }}
+      >
+        {darkMode ? "☀ Light Mode" : "🌙 Dark Mode"}
+      </button>
     </div>
   );
 }
